c8d784c405821117c8e47bbd04442a1683c60ba6
AkAsH <159448418+akashverma-1@users.noreply.github.com>
2024-03-01 03:15:48 -0800

    first commit

---
+++ loop1.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+pencolor('blue')
+pensize(3)
+speed('slowest')
+for i in range(9):
+    fd(120)
+    lt(40)
+    write(i, font=('calibri',20))
+
+
+
+
+
+
+hideturtle()
+
+mainloop()
+
+
